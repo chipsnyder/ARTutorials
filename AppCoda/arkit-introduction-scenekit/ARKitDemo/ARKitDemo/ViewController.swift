819576bd085e409a4a87decc7a7d9494eedbfe2e
Chip Snyder <chip.snyder3@gmail.com>
2018-06-02 12:39:16 -0400

    init base app

---
+++ AppCoda/arkit-introduction-scenekit/ARKitDemo/ARKitDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ARKitDemo
+//
+//  Created by Chip Snyder on 6/2/18.
+//  Copyright © 2018 EisbarDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
